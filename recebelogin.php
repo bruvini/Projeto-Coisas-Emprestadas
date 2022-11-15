0f90a3589b088cee0a515f68036ac0975d1b47fd
bruvini <86611700+bruvini@users.noreply.github.com>
2022-11-14 21:55:00 -0300

    Tentativa de acionar o login

--- recebelogin.php
+++ recebelogin.php
@@ -1,6 +1,7 @@
 <?php
 
-    echo "Dados recebidos<br>";
+//echo "Dados recebidos<br>";
 
-    echo "E-mail: ". $_POST["mail"]. "<br>";
-    echo "Senha: ". $_POST["senha"]. "<br>";
+//echo "E-mail: ". $_POST["mail"]. "<br>";
+//echo "Senha: ". $_POST["senha"]. "<br>";
+
