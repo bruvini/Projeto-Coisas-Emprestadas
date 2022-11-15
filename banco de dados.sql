bca7297dc451383f32733fa1021916a9c0a09ac8
bruvini <86611700+bruvini@users.noreply.github.com>
2022-11-15 17:35:54 -0300

    Script mysql salvo

--- banco de dados.sql
+++ banco de dados.sql
@@ -9,20 +9,22 @@ sexoUser varchar(10)
 );
 
 delete from tbUsuarios where senhaUser = '.admin123.';
+select mailUser from tbUsuarios where mailUser = "bruvini.silva12@gmail.com";
 select * from tbUsuarios;
+select * from emprestimo;
+select * from cadastroProdutos;
 
 create table cadastroProdutos(
 nomeProd varchar(50) primary key not null,
 estadoProd varchar(9) not null
 );
 
+drop table emprestimo;
 create table emprestimo(
 idEmprestimo int primary key auto_increment,
 nomeProd varchar(50) not null,
 nomeUser varchar(50) not null,
 fone varchar(11) not null,
 dataEmprestimo timestamp null default current_timestamp,
-dataDevolucao varchar(10) not null,
-foreign key (nomeProd) references cadastroProdutos (nomeProd),
-foreign key (nomeUser) references tbUsuarios (nomeUser)
+dataDevolucao varchar(10) not null
 );
